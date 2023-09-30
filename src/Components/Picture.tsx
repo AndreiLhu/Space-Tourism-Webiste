f18cac2376704689e243c296628599216e3d4d70
AndreiLhu <leahu.andreilucian@gmail.com>
2023-09-30 11:32:14 +0200

    Added destination page and components

---
+++ src/Components/Picture.tsx
@@ -0,0 +1,22 @@
+type tPictureProps = {
+  alt: string;
+  className?: string;
+  pngPicture: string;
+  webpPicture: string;
+};
+
+const Picture = ({
+  alt,
+  className = '',
+  pngPicture,
+  webpPicture,
+}: tPictureProps) => {
+  return (
+    <>
+      <source srcSet={webpPicture} type="image/webp" />
+      <img alt={alt} className={className} src={pngPicture} />
+    </>
+  );
+};
+
+export default Picture;
--- src/Pages/Destination/Destination.tsx
+++ src/Pages/Destination/Destination.tsx
@@ -1,12 +1,46 @@
+import React from 'react';
 import Navbar from '../../Components/Navbar/Navbar';
 import './destination.css';
+import data from '../../data/destinations.json';
+import Picture from '../../Components/Picture';
+import DestinationsInfo from '../../Components/DestinationsInfo';
 
 const Destination: React.FC = () => {
+  const { destinations } = data;
+  const [planetId, setPlanetId] = React.useState<number>(0);
+
+  const onChangePlanet = React.useCallback((index: number) => {
+    setPlanetId(index);
+  }, []);
+
   return (
-    <>
+    <div className="destinationBackground">
       <Navbar />
-      <h1>Destination</h1>
-    </>
+      <h1 className="mainTitle">01 Pick your destination</h1>
+      <ul>
+        {destinations.map((planetInfo, index) => (
+          <button key={index} onClick={() => onChangePlanet(index)}>
+            {planetInfo.name}
+          </button>
+        ))}
+      </ul>
+
+      <div>
+        <Picture
+          alt={destinations[planetId].name}
+          pngPicture={destinations[planetId].images.png}
+          webpPicture={destinations[planetId].images.webp}
+        />
+        <div className="destinationInfoContainer">
+          <DestinationsInfo
+            name={destinations[planetId].name}
+            description={destinations[planetId].description}
+            distance={destinations[planetId].distance}
+            travelTime={destinations[planetId].travel}
+          />
+        </div>
+      </div>
+    </div>
   );
 };
 export default Destination;
