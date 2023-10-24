ef79558e0548ec8769c51bca67a7d4b9ba9903b4
AndreiLhu <leahu.andreilucian@gmail.com>
2023-10-24 11:41:56 +0200

    removing the import in app

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,10 @@ import Destination from './Pages/Destination/Destination';
 import Home from './Pages/Home/Home';
 import { Route, Routes } from 'react-router';
 import Technology from './Pages/Technology/Technology';
-import MobileNavbar from './Components/MobileNavbar/MobileNavbar';
 
 function App() {
   return (
     <React.Fragment>
-      {/* <MobileNavbar /> */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/crew" element={<Crew />} />
