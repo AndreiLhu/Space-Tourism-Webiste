308a3da973493b4c5d0e2dd86783d4d584538e74
AndreiLhu <leahu.andreilucian@gmail.com>
2023-10-31 16:00:38 +0100

    small change

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,5 +1,5 @@
-import { Link } from 'react-router-dom';
 import './navbar.css';
+import { Link } from 'react-router-dom';
 import logo from '../../../src/assets/shared/logo.svg';
 
 const Navbar: React.FC = () => {
